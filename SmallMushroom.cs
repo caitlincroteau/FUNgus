5c1aa092c4944f54d4ccdd8abf9feef34c47213f
Caitlin Croteau <cjcroteau@gmail.com>
2023-03-14 21:44:39 -0700

    initial commit

---
+++ SmallMushroom.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallMushroom : MonoBehaviour
+{
+    public float MushroomSpeed = 10f;
+    public bool goingRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    
+    {
+        if(goingRight) {
+            transform.position += Vector3.right * MushroomSpeed * Time.deltaTime;
+        } else {
+            transform.position += Vector3.left * MushroomSpeed * Time.deltaTime;
+        }
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "SmallMushroom") {
+            Destroy(other.gameObject);
+        } 
+    }
+}
